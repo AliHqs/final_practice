cb3bbed094e27d739f2d2e0da2a5a64eb99482bb
ali hassan <l1f21bsse0281@ucp.edu.pk>
2025-02-19 02:24:56 +0500

    initial commit

---
+++ configuration.sh
@@ -0,0 +1,7 @@
+echo "Repo initial configuration is started"
+git config --local user.name "ali hassan"
+git config --local user.email "l1f21bsse0281@ucp.edu.pk"
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/style.css .gitignore
+npm init -y
